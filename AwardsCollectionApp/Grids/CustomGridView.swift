f1aa4f1939ce0a9b99013d72b91ebb619d1217cb
Artem Pavlov <pavlov.tema@gmail.com>
2021-11-24 22:04:32 +0500

    Content with itemSize.

--- AwardsCollectionApp/Grids/CustomGridView.swift
+++ AwardsCollectionApp/Grids/CustomGridView.swift
@@ -11,22 +11,21 @@ import SwiftUI
 struct CustomGridView<Content, T>: View where Content: View { //чтобы сделать универсальную сетку, чтобы она принимала любой тип данных (дженерик), мы определяем тип через Т
     let items: [T] //задаем тип даных Т
     let columns: Int // определяем количество столбцов
-    let content: (T) -> Content //захватывает тип данных и возвращает View
+    let content: (CGFloat, T) -> Content //захватывает тип данных и возвращает View, и возвращаем размер ячейки CGFloat
     var rows: Int { //вычисляемое количество строк
         items.count / columns
     }
     
     var body: some View {
         GeometryReader { geometry in // для работы с размерами ячеек
-            let sideLength = geometry.size.width / CGFloat(columns) //определяем размер одной стороны. Берем ширину экрана и делим на количество колонок
+            let itemSize = geometry.size.width / CGFloat(columns) //определяем размер одной стороны. Берем ширину экрана и делим на количество колонок
             ScrollView {
                 VStack { //делаем строки
                     ForEach(0...rows, id: \.self) { rowIndex in //равное количеству строк. id - определяем индификатор элемента коллекции
                         HStack { //делаем столбцы
                             ForEach(0..<columns) { columnIndex in //равное количеству столбцов
                                 if let index = indexFor(row: rowIndex, column: columnIndex) { //если мы сможем извлечь опциональное значение из метода, то тогда мы отобразим в виде текста
-                                    content(items[index]) //отображаем контент
-                                        .frame(width: sideLength, height: sideLength) //делаем квадратики из ячеек
+                                    content(itemSize, items[index]) //отображаем контент
                                 } else {
                                     Spacer() //чтобы сдвинуть единицу влево
                                 }
@@ -48,8 +47,9 @@ struct CustomGridView_Previews: PreviewProvider {
     static var previews: some View {
    
         CustomGridView(
-            items: [11, 3, 7, 17, 5, 2, 1], columns: 3) { item in
+            items: [11, 3, 7, 17, 5, 2, 1], columns: 3) { itemSize, item in
             Text("\(item)")
+                    .frame(width: itemSize, height: itemSize)
         }
     }
 }
